06ce6994bbb1079eea1b40c6cc274f4cccffea88
LeeSeongDo <dltjdgns3z@naver.com>
2023-11-15 11:06:48 +0900

    Portfolio1 Finish

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 섹션 04 Next.js에 대해 학습
 => 포트폴리오 과제 11.15일 마감.
+
+<<에러 해결 >> 
+ git 403 에러
+ git remote set-url origin https://github.com/CaliforniaLuv/YUMMY-SEOUL.git
--- pages/Portfolio/section01/index.js
+++ pages/Portfolio/section01/index.js
@@ -1,4 +1,4 @@
-import {FlexBox} from "../../../styles/01-portfolio";
+import {BigInputStyle, FinalButton, ColumnBox, FlexBox, FontStyle2, RadioButton, LongColumnBox, MediumInputStyle, nputStyle, RowBox, RowBox2, SearchButton, SmallInputStyle, FontStyle3} from "../../../styles/01-portfolio";
 
 export default function Portfolio() {
     return (
@@ -6,68 +6,70 @@ export default function Portfolio() {
             <h1>게시물 등록</h1>
 
             {/* 첫번째 flex박스 */}
-            <div>
-                <div>
-                    <span>작성자</span>
-                    <input type="text" />
-                </div>
+            <RowBox>
+                <ColumnBox>
+                    <FontStyle2>작성자</FontStyle2>
+                    <MediumInputStyle type="text" placeholder="이름을 적어주세요."/>
+                </ColumnBox>
 
-                <div>
-                    <span>작성자</span>
-                    <input type="text" />
-                </div>
-            </div>
+                <ColumnBox>
+                    <FontStyle2>비밀번호</FontStyle2>
+                    <MediumInputStyle type="text" placeholder="비밀번호를 입력해주세요"/>
+                </ColumnBox>
+            </RowBox>
 
             {/* 두번째 flex 박스 */}
-            <div>
-                <span>제목</span>
-                <input type="text" />
-            </div>
+            <LongColumnBox>
+                <FontStyle2>제목</FontStyle2>
+                <MediumInputStyle type="text" placeholder="제목을 입력해주세요" />
+            </LongColumnBox>
 
             {/* 세번째 flex 박스 */}
-            <div>
-                <span>내용</span>
-                <input type="text" />
-            </div>
+            <LongColumnBox>
+                <FontStyle2>내용</FontStyle2>
+                <BigInputStyle type="text" placeholder="내용을 입력해주세요."/>
+            </LongColumnBox>
 
             {/* 네번째 flex 박스 */}
-            <div>
-                <span>주소</span>
+            <LongColumnBox>
+                <FontStyle2>주소</FontStyle2>
                 {/* 우편번호 검색 div */}
-                <div>
-                    <input type="number" />
-                    <button>우편번호 검색</button>
-                </div>
-                <input type="text" />
-                <input type="text" />
-            </div>
+                <RowBox2>
+                    <SmallInputStyle type="text" placeholder="07250" />
+                    <SearchButton>우편번호 검색</SearchButton>
+                </RowBox2>
+                <MediumInputStyle type="text" />
+                <MediumInputStyle type="text" />
+            </LongColumnBox>
 
             {/* 다섯번째 flex 박스 */}
-            <div>
-                <span>유튜브</span>
-                <input type="text" />
-            </div>
+            <LongColumnBox>
+                <FontStyle2>유튜브</FontStyle2>
+                <MediumInputStyle type="text" />
+            </LongColumnBox>
 
             {/* 여섯번째 flex 박스 */}
-            <div>
-                <span>사진 첨부</span>
-                <div>
+            <LongColumnBox>
+                <FontStyle2>사진 첨부</FontStyle2>
+                <RowBox2>
+                    <input type="file"/>
                     <button>Upload</button>
                     <button>Upload</button>
-                    <button>Upload</button>
-                </div> 
-            </div>
+                </RowBox2> 
+            </LongColumnBox>
             {/* 일곱번째 flex 박스 */}
-            <div>
-                <span>메인 설정</span>
-                <div>
-                    <input type="radio" name="one"/>유튜브
-                    <input type="radio" name="one"/>사진
-                </div>
-            </div>
+            <LongColumnBox>
+                <FontStyle2>메인 설정</FontStyle2>
+                <RowBox2>
+                    <RadioButton type="radio" name="one"/>
+                    <FontStyle3>유튜브</FontStyle3>
+                    <RadioButton type="radio" name="one"/>
+                    <FontStyle2>사진</FontStyle2>
+                </RowBox2>
+            </LongColumnBox>
             {/* 마지막 flex 박스 */}
             <div>
-                <button>등록하기</button>
+                <FinalButton>등록하기</FinalButton>
             </div>
         </FlexBox>
     )
