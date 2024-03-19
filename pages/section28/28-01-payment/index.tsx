3e428f26fb3f76c4c1f62d464a98d7f44813f059
LeeSeongDo <dltjdgns3z@naver.com>
2024-03-19 21:20:24 +0900

    Day0319(2)

--- pages/section28/28-01-payment/index.tsx
+++ pages/section28/28-01-payment/index.tsx
@@ -5,43 +5,46 @@ declare const window: typeof globalThis & {
 
 export default function PaymentPage(): JSX.Element {    
     
-    const onClickPayment = (): void => {
+    function OpenPage() {
         const IMP = window.IMP;
-        IMP.init("imp55075685")
-        
+        IMP.init('imp55075685');
+
         IMP.request_pay({
-            pg: "nictest00m",
-            pay_method: "card",
-            //merchant_uid: "결제 번호",
-            name: "아이리버 무선 마우스 외 1개",
-            amount: 10000,
-            buyer_email: "dltjdgns3z@gmail.com",
-            buyer_name: "홍길동",
-            buyer_tel: "010-4242-4242",
-            buyer_addr: "서울특별시 강남구 신사동",
-            buyer_postcode: "01181",
-            m_redirect_url : "/"
-                
-        }, function (rsp) { // callback
-                        if (rsp.success) {
-                            alert('결제가 성공했습니다.');
-                            // 결제 성공 로직
-                        } else {
-                            console.log(rsp)
-                            alert('결제에 실패했습니다.');
-                            // 결제 실패 로직
-                        }
+            pg : 'tosspayments',
+            pay_method : 'card',
+            // merchant_uid: "order_no_0001", //상점에서 생성한 고유 주문번호
+            name : '주문명:결제테스트',
+            amount : 14000,
+            buyer_email : 'iamport@siot.do',
+            buyer_name : '구매자이름',
+            buyer_tel : '010-1234-5678',
+            buyer_addr : '서울특별시 강남구 삼성동',
+            buyer_postcode : '123-456',
+            m_redirect_url : '{모바일에서 결제 완료 후 리디렉션 될 URL}', // 예: https://www.my-service.com/payments/complete/mobile
+            niceMobileV2 : true // 신규 모바일 버전 적용 시 설정
+        }, function(rsp) { // callback 로직
+            //* ...중략 (README 파일에서 상세 샘플코드를 확인하세요)... *//
+            if(rsp.success) {
+                console.log(rsp)
+            } else {
+                console.log(rsp)
+            }
         });
 
     }
-    
+
 
     return (
         <>
-            <script src="https://cdn.iamport.kr/v1/iamport.js"></script>
-            
-            <div>
-                <button onClick={onClickPayment}>결제하기</button>
+           <script src="https://cdn.iamport.kr/v1/iamport.js"></script>
+            <script>
+                
+            </script>    
+
+
+            <div style={{width: 300, height: 200, border: "1px solid black", borderRadius: "10px", margin: "10px"}}>
+                <p>아이리버 무선 마우스 외 1개</p>
+                <button onClick={OpenPage} >결제하기</button>
             </div>
         </>
     )
